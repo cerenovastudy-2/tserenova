cda29a0eec026d66097f65f91f0f7d243dbac8f3
cerenovastudy-2 <cerenovastudy22@gmail.com>
2025-09-26 19:11:21 +0300

    hash.java

--- java-2.java
+++ java-2.java
@@ -1,64 +1,13 @@
-//Реализация хэш-таблиц
-import java.util.LinkedList;
+import java.util.HashMap;
 
-public class HashTable<K, V> {
-    private class Pair {
-        K key;
-        V value;
-        
-        Pair(K key, V value) {
-            this.key = key;
-            this.value = value;
+public class Main {
+    public static void main(String[] args) {
+        String text = "hello";
+        HashMap<Character, Integer> freq = new HashMap<>();
+
+        for (char c : text.toCharArray()) {
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
         }
-    }
-    
-    private LinkedList<Pair>[] table;
-    private int capacity;
-    
-    @SuppressWarnings("unchecked")
-    public HashTable(int capacity) {
-        this.capacity = capacity;
-        table = new LinkedList[capacity];
-        for (int i = 0; i < capacity; i++) {
-            table[i] = new LinkedList<>();
-        }
-    }
-    
-    private int hashFunction(K key) {
-        return Math.abs(key.hashCode()) % capacity;
-    }
-    
-    public void put(K key, V value) {
-        int index = hashFunction(key);
-        for (Pair pair : table[index]) {
-            if (pair.key.equals(key)) {
-                pair.value = value;
-                return;
-            }
-        }
-        table[index].add(new Pair(key, value));
-    }
-    
-    public V get(K key) {
-        int index = hashFunction(key);
-        for (Pair pair : table[index]) {
-            if (pair.key.equals(key)) {
-                return pair.value;
-            }
-        }
-        throw new RuntimeException("Key not found");
-    }
-    
-    public void remove(K key) {
-        int index = hashFunction(key);
-        for (Pair pair : table[index]) {
-            if (pair.key.equals(key)) {
-                table[index].remove(pair);
-                return;
-            }
-        }
-        throw new RuntimeException("Key not found");
+        System.out.println(freq); // {h=1, e=1, l=2, o=1}
     }
 }
-
-
