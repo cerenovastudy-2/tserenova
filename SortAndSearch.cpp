c74e29924ef2a65596169c2f9daf9f5cf53eb950
cerenovastudy-2 <cerenovastudy22@gmail.com>
2025-10-18 10:11:27 +0300

    Update SortAndSearch.cpp

--- SortAndSearch.cpp
+++ SortAndSearch.cpp
@@ -72,6 +72,36 @@ void heapSort(std::vector<int>& arr) {
         heapify(arr, i, 0);
     }
 }
-9. Бинарный поиск (Binary Search) - C++
-    
+//9. Бинарный поиск (Binary Search) - C++
+    #include <iostream>
+#include <vector>
+
+int binarySearch(const std::vector<int>& arr, int target) {
+    int left = 0;
+    int right = arr.size() - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) return mid;
+        if (arr[mid] > target) right = mid - 1;
+        else left = mid + 1;
+    }
+    return -1;
+}
+
+//Интерполяционный поиск (Interpolation Search) - C++
+#include <iostream>
+#include <vector>
+
+int interpolationSearch(const std::vector<int>& arr, int target) {
+    int low = 0;
+    int high = arr.size() - 1;
+    while (low <= high && target >= arr[low] && target <= arr[high]) {
+        int pos = low + (((double)(high - low) / (arr[high] - arr[low])) * (target - arr[low]));
+        if (arr[pos] == target) return pos;
+        if (arr[pos] < target) low = pos + 1;
+        else high = pos - 1;
+    }
+    return -1;
+}
+
 
